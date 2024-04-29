b5fc371d1ed7f5f8b285556a4a3ae13f9c2da5ed
pedroronchini <pe-ronchini@hotmail.com>
2024-04-29 12:46:25 -0300

    edit project

--- src/br/com/alura/screenmatch/principal/PrincipalComBusca.java
+++ src/br/com/alura/screenmatch/principal/PrincipalComBusca.java
@@ -5,12 +5,20 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.Scanner;
 
 public class PrincipalComBusca {
     public static void main(String[] args) throws IOException, InterruptedException {
+        Scanner leitura = new Scanner(System.in);
+
+        System.out.println("Digite um filme para busca: ");
+        var busca = leitura.nextLine();
+
+        String endereco = "http://www.omdbapi.com/?t="+ busca + "&apikey=7a528f37";
+        
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder()
-            .uri(URI.create("http://www.omdbapi.com/?t=matrix&apikey=7a528f37"))
+            .uri(URI.create(endereco))
             .build();
 
         HttpResponse<String> response = client
